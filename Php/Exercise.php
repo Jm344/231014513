50e3f76d0c9fe0fc1003793acfd4552af9e51f27
M303User <M303User@m303.uc-bcf.edu.ph>
2025-03-14 14:03:49 +0800

    Added Php File

--- Php/Exercise.php
+++ Php/Exercise.php
@@ -28,8 +28,49 @@
         }
 
         echo "<br> Exercise 3";
+        echo "<br> FizzBuzz:";
+        for ($i = 1; $i <= 100; $i++) {
+            if ($i % 3 == 0 && $i % 5 == 0) {
+                echo " FizzBuzz";
+            } elseif ($i % 3 == 0) {
+                echo " Fizz";
+            } elseif ($i % 5 == 0) {
+                echo " Buzz";
+            } else {
+                echo " $i";
+            }
+        }
+
+        echo "<br><br> Even Fibonacci Numbers:";
+        $a = 0;
+        $b = 1;
+        for ($i = 0; $i < 10; $i++) {
+            $next = $a + $b;
+            if ($next % 2 == 0) {
+                echo " $next";
+            }
+            $a = $b;
+            $b = $next;
+        }
+
+
+        echo "<br> Exercise 4";
+        function greet($name) {
+            return "Hello, $name!";
+        }
         
-        ?>
+        function square($num) {
+            return $num * $num;
+        }
+        
+        echo greet("Ayisha") . "\n";
+        echo square(4) . "\n";
+
+        echo "<br> Exercise 5";
+            
+
+
+    ?>
         
 </body>
 </html>
